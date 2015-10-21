8fafad3d8dc7162c2228804c643787dbfb54dc9b
Eric N. Garcia <engarcia@us.ibm.com>
2015-10-21 14:53:27 -0400

    Removed adde <p/> from headers.

--- android/mfp-bluelist-on-premises/app/src/main/java/com/ibm/bluelist/BlueListApplication.java
+++ android/mfp-bluelist-on-premises/app/src/main/java/com/ibm/bluelist/BlueListApplication.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 IBM Corp.
- * <p/>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,7 +59,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
-public class BlueListApplication extends Application {
+public class dBlueListApplication extends Application {
 
     private static final String TAG = BlueListApplication.class.getCanonicalName();
     private static final String TODO_ITEM_INDEX_NAME = "todoItem_index";
